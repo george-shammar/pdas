5509f7609a7d99d04927fbe55b7bb85017b31c98
George Gbenle <george.gbenle@comms365.com>
2023-02-28 09:24:16 +0000

    Create component

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import REACT from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"
+
+
+const el = Document.getElementById("root");
+const root = ReactDOM.createRoot(el);
+
+
+root.render(<App />);
