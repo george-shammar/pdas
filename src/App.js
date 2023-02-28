a4e57f51394c09ea549a1bc1bca5a123557b917f
George Gbenle <george.gbenle@comms365.com>
2023-02-28 09:17:58 +0000

    Create component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import ProfileCard from "./ProfileCard";
+
+
+function App() {
+  return <div>
+    <div>PROFILE HEADER</div>
+    <div>
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
+    </div>
+  </div>
+};
+
+export default App;
